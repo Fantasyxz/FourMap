31fad151e341c09b09f3797edf465dbff0869705
sabilfaustaa <61264687+sabilfaustaa@users.noreply.github.com>
2020-11-11 11:01:38 +0700

    New Project
    Ulang gan

---
+++ Android/app/src/main/java/com/firebase/smartschool/RegisterActivity.java
@@ -0,0 +1,80 @@
+package com.firebase.smartschool;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText inputUsername, inputPass;
+    private Button btnLogin;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        inputUsername = findViewById(R.id.inputUsername);
+        inputPass = findViewById(R.id.inputPass);
+
+        // Inisialisasi Register
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = inputUsername.getText().toString();
+                String password = inputPass.getText().toString();
+
+                if (username.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "Username tidak boleh kosong",
+                            Toast.LENGTH_SHORT).show();
+                } else if (password.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "Password tidak boleh kosong",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.createUserWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        Toast.makeText(RegisterActivity.this, "Authentication success.",
+                                                Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                    // ...
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent goLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(goLogin);
+        finish();
+    }
+}
