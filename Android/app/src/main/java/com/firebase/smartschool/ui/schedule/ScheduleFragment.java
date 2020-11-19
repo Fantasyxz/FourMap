50123d816a5332934996e431bc238ab2992e7012
NeroSnowblade <fajarmustaqim11@gmail.com>
2020-11-19 14:37:45 +0700

    Idk what Commit is this

--- Android/app/src/main/java/com/firebase/smartschool/ui/schedule/ScheduleFragment.java
+++ Android/app/src/main/java/com/firebase/smartschool/ui/schedule/ScheduleFragment.java
@@ -1,4 +1,4 @@
-package com.firebase.smartschool.ui.slideshow;
+package com.firebase.smartschool.ui.schedule;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.firebase.smartschool.R;
 
-public class SlideshowFragment extends Fragment {
+public class ScheduleFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private ScheduleViewModel scheduleViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        scheduleViewModel =
+                new ViewModelProvider(this).get(ScheduleViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        final TextView textView = root.findViewById(R.id.text_gallery);
+        scheduleViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
