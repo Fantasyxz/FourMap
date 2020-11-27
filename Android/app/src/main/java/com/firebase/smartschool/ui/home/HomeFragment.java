5cd3df338c82a79ceff1b3b81479c55103afc468
NeroSnowblade <fajarmustaqim11@gmail.com>
2020-11-27 14:40:03 +0700

    just Logo

--- Android/app/src/main/java/com/firebase/smartschool/ui/home/HomeFragment.java
+++ Android/app/src/main/java/com/firebase/smartschool/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,20 +18,14 @@ import com.firebase.smartschool.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        WebView myWebView = (WebView) root.findViewById(R.id.webview);
+        myWebView.loadUrl("https://developer.android.com/guide/webapps/webview");
+        myWebView.setWebViewClient(new WebViewClient());
+
         return root;
     }
 }
