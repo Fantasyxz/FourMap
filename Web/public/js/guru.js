f744ee7b9d95262c2cab69678d5451e5984a6360
NeroSnowblade <fajarmustaqim11@gmail.com>
2020-12-05 18:31:10 +0700

    finish - sabil
    yeay

--- Web/public/js/guru.js
+++ Web/public/js/guru.js
@@ -12,6 +12,7 @@ $("#btn-register").click(function(){
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         firebase.database().ref('guru/' + user.uid).set({
+          id : user.uid,
           nip : valNip,
           nama : valNama,
           gender : valGender,
@@ -27,18 +28,19 @@ $("#btn-register").click(function(){
   });
 });
 
-
+//Button Logout
 $("#btn-logout").click(function(){
   firebase.auth().signOut();
   window.location.href = "login.html";
 });
+
 
 // Read Data Function
 var guruRef = firebase.database().ref().child('guru');
 guruRef.on("value", function(snapshot){
   $("#data-guru").empty();
   var content = "";
-
+  
   snapshot.forEach(function(childsnapshot){
     var x = childsnapshot.val();  
     content += "<tr>";
@@ -47,11 +49,17 @@ guruRef.on("value", function(snapshot){
     content +=	"<td>" + x.email + "</td>";
     content +=	"<td>"+ x.gender +"</td>";
     content +=	"<td>" + x.bidang + "</th>";
-    content +=	"<td class='d-flex'>"
-    content += 		"<a href='#' class='btn btn-delete btn-danger btn-sm mr-1' id='btn-delete'><i class='fas fa-trash fa-sm text-light'></i></a>";
-    content +=  	"<a href='#' class='btn btn-edit btn-warning btn-sm' id='btn-edit'><i class='fas fa-edit fa-sm text-light'></i></a>";
+    content +=	"<td class='d-flex'>";
+    content += 		"<button class='btn btn-danger btn-sm mr-1 btn-delete' id='" + x.id + "'><i class='fas fa-trash fa-sm text-light'></i></button>";
+    content +=  	"<a href='#' class='btn btn-edit btn-warning btn-sm' data-toggle='modal' data-target='#modalEdit' id='btn-edit'><i class='fas fa-edit fa-sm text-light'></i></a>";
     content += 	"</td>"
     content += "</tr>";
   });
   $("#data-guru").html(content);
+});
+
+// Delete Function 
+$(document).on("click",".btn-delete",function(){
+  var userID = $(this).attr('id');
+  firebase.database().ref("guru/" + userID).remove();
 });
